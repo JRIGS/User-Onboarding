341dd11459f671d8c0d63b30c8ed0931d106606f
JRIGS <raczkowskijonathan@gmail.com>
2020-08-19 21:32:00 -0400

    Finished up to YUP

--- advanced-form/src/App.js
+++ advanced-form/src/App.js
@@ -1,23 +1,136 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
 import axios from 'axios'
 import * as yup from 'yup'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import formSchema from './validation/formSchema';
+
+
+  const initialDisabled = true
+
+  const initialFormErrors = {
+    username: '',
+    email: '',
+    password: '',
+    tos: '',
+  }
 
 
 function App() {
 
   const [memberInfo, setMemberInfo] = useState({name: "", email: "", password: "", tos: false});
   const [members, setMembers] = useState([])
+
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled) 
+
   
+const checkboxChange = (tos, isChecked) => {
+  if(memberInfo.tos === false){
+    setMemberInfo({
+      ...memberInfo,
+      [tos]: isChecked
+    }
+    )
+  }
+  else {
+    setMemberInfo({
+      ...memberInfo,
+      [tos]: false
+    })
+  }
+}
+
+const inputChange = (name, value) => {
+  yup
+  .reach(formSchema, name)
+  
+  .validate(value)
+
+  .then(valid => {
+    setFormErrors({
+      ...formErrors,
+      [name]: ""
+    });
+  })
+
+  .catch(err => {
+    setFormErrors({
+      ...formErrors,
+      [name]: err.errors[0],
+    });
+  });
+
+  setMemberInfo({
+    ...memberInfo,
+    [name]: value
+  })
+}
+
+const submit = () => {
+  const newUser = {
+    username: memberInfo.name.trim(),
+    email: memberInfo.email.trim(),
+    password: memberInfo.password.trim()
+  }
+  postNewMember(newUser)
+  //postNewMember is made below
+}
+
+const postNewMember = newMember => {
+
+  axios.post('https://reqres.in/api/users', newMember)
+  .then(res => {
+    setMembers([...members, res.data])
+    console.log(res.data);
+  })
+  .catch(err => {
+    debugger
+  })
+  // .finally (() =>{
+  //   setMembers(memberInfo)
+  // })
+}
+
+useEffect(() => {
+  formSchema.isValid(memberInfo)
+  .then(valid => {
+    setDisabled(!valid);
+  })
+}, [memberInfo])
+
+
 
 
   return (
-   <Form />
+    <div className='container'>
+
+   <Form 
+   memberInfo={memberInfo} 
+   checkboxChange={checkboxChange} 
+   inputChange={inputChange} 
+   submit={submit}
+   disabled={disabled}
+   formErrors={formErrors}
+   />
+
+   {
+      members.map(member => {
+          return (
+         
+              <section className="member-card">
+              <h1>{member.name}</h1>
+              <h1>{member.email}</h1>
+              <h1>{member.password}</h1>
+              </section>
+             )
+          })
+        }
+  </div>
      
   );
+
 }
 
 export default App;
--- advanced-form/src/components/Form.js
+++ advanced-form/src/components/Form.js
@@ -3,29 +3,69 @@ import '../App.css';
 
 function Form(props) {
 
+const {memberInfo, checkboxChange, inputChange, submit, disabled, formErrors} = props
+
+const onCheckboxChange = evt => {
+    const { name, value } = evt.target
+    // console.log(evt.target);
+    checkboxChange(name, value)
+  }
+
+const onInputChange = evt => {
+    const { name, value } = evt.target
+    // console.log(evt.target);
+    inputChange(name, value)
+}
+
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+  }
+
+
 return (
 
     <form>
+        <div className="errors-container">
+          <div>{formErrors.username}</div>
+          <div>{formErrors.password}</div>
+          <div>{formErrors.email}</div>
+          <div>{formErrors.tos}</div>
+        </div>
+
          <label>
             <br />
             Name: <input
             type="text"
             name="name"
+            placeholder="type name here..."
+            value={memberInfo.name}
+            onChange={onInputChange}
             />
             Email: <input
             type="email"
             name="email"
+            value={memberInfo.email}
+            onChange={onInputChange}
+            placeholder="type email here..."
             />
             Password: <input
-            type="text"
+            type="password"
             name="password"
+            value={memberInfo.password}
+            onChange={onInputChange}
+            placeholder="type password here..."
             />
             TOS: <input
             type="checkbox"
-            name="password"
+            name="tos"
+            checked={memberInfo.tos === 'true'}
+            value={true}
+            onChange={onCheckboxChange}
             />
         </label>
-        <button>Submit</button>
+        <button disabled={disabled} onClick={onSubmit}>Submit</button>
+        
     </form>
 )
 }
